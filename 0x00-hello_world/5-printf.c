2a16dcedbb8c9f55b844b9f77de78035f2952e9e
77679 <106762511+77679@users.noreply.github.com>
2022-07-06 06:21:45 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,27 +1,10 @@
 #include <stdio.h>
-
-
-
-/
-
- * main - prints some text
-
- *
-
- * Return: always 0(success)
-
- */
-
-
-
+/**
+* main - prints some text
+* Return: always 0(success)
+*/
 int main(void)
-
 {
-
 printf("with proper grammar, but the outcome is a piece of art,\n");
-
 return (0);
-
 }
-
-
