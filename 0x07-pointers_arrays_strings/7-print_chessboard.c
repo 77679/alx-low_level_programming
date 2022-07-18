1e6b0ba2cdbf692a330205d93913a663a2dbe3ed
77679 <106762511+77679@users.noreply.github.com>
2022-07-18 09:57:16 -0700

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* print_chessboard - prints the chessboard
-* @a: the board to print
+* print_chessboard - Prints chessboard
+* @a: Double pointer
+* Return: 0
 */
 void print_chessboard(char (*a)[8])
 {
-int i, j;
-i = j = 0;
-while (i < 8)
+unsigned int i, j;
+for (i = 0; i < 8; i++)
 {
-j = 0;
-while (j < 8)
-{
+for (j = 0; j < 8; j++)
 _putchar(a[i][j]);
-j++;
-}
 _putchar('\n');
-i++;
-} 
+}
 }
