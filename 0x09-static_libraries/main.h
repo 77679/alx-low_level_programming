006b0710397dd27f2821c230f660f4ac06428165
77679 <106762511+77679@users.noreply.github.com>
2022-07-22 12:48:18 -0700

    Create main.h

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,23 @@
+int putchar(char c);
+int_islower(int c);
+intisalpha(int c);
+int _abs (int n);
+isupper(int c);
+int isdigit (int c);
+int _strlen( char *s);
+void puts (char *S);
+char *_strcpy(char *dest, char *S rc);
+int atoi(char *s);
+int
+char *_strcat( char *dest, char *src);
+char *Strncat (char *dest, char *Src, int n)
+char *_strncpy(char *dest, char *src, int n)
+intstrcmp( char *s1, char *s2);
+char *_memset (char *S, char b, unsigned int
+n)
+char *_memcpy(char *dest, char *S rC, unsigne
+d int n);
+char*strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char st rpbrk(char *5, char *accept);
+char _strstr(char *haystack, char *needle);
