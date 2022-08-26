af12825c0bc334126552946799081d7766f95f3d
77679 <106762511+77679@users.noreply.github.com>
2022-08-26 14:18:54 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * create_file - check the code for Holberton School students.
- * @filename: name of my file
- * @text_content: number of the letters that i used
- * Return: Always 0.
+ * create_file - creates a file
+ * @filename:name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *Return: 1 on success, -1 on failure
+ *
+ *The created file must have those permissions: rw-------.
+ *If the file already exists, do not change the permissions.
+ *if the file already exists, truncate it
+ *if filename is NULL return -1
+ *if text_content is NULL create an empty file
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int ID = 0, wr = 0, count = 0;
+	int fd, i = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	if (text_content != NULL)
+	/* write or create if nonexistant */
+	/* truncate to zero bytes if existant */
+	/* permission : S_I | R, W, X | USR, GRP, OTH */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd == -1)
+			return (-1);
+
+	if (text_content)
 	{
-		for (count = 0; text_content[count] != '\0'; count++)
-		{}
+		while (text_content[i]) /* string length */
+			i++;
+		if (write(fd, text_content, i) != i)
+			return (-1);
 	}
-	ID = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (ID == -1)
-	{
-		return (-1);
-	}
-	wr = write(ID, text_content, count);
-	if (wr == -1)
-	{
-		return (-1);
-	}
-	close(ID);
+	close(fd);
 	return (1);
 }
